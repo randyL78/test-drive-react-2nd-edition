7ea857f4df89443d0bcdf47f3ff78ad6923592d9
Randy Layne <randydavidl78@gmail.com>
2024-06-26 12:44:04 -0400

    Finish FizzBuzz example

--- fizzbuzz/index.test.js
+++ fizzbuzz/index.test.js
@@ -9,4 +9,12 @@ describe('fizzBuzz()', () => {
     expect(fizzBuzz(15)).toBe('FizzBuzz');
     expect(fizzBuzz(30)).toBe('FizzBuzz');
   })
+
+  it('returns "Fizz" for multiples of 3', () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+  })
+
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  })
 })
