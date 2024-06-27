698d39171d0b8ba6df0f6c883145886367b9da70
Randy Layne <randydavidl78@gmail.com>
2024-06-27 14:54:41 -0400

    :sparkles: Add next and previous buttons to Carousel component

--- carousel/src/Carousel.test.tsx
+++ carousel/src/Carousel.test.tsx
@@ -1,6 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import Carousel from "./Carousel";
 import {expect} from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe('Carousel', () => {
   const slides = [
@@ -31,6 +32,41 @@ describe('Carousel', () => {
     render(<Carousel slides={slides} />)
     const img = screen.getByRole('img')
 
+    expect(img).toHaveAttribute('src', slides[0].imgUrl)
+  })
+
+  it('advances the slide when the Next button is clicked', async () => {
+    render(<Carousel slides={slides} />)
+    const img = screen.getByRole('img')
+    const nextButton = screen.getByTestId('next-button')
+    const user = userEvent.setup()
+
+    await user.click(nextButton)
+    expect(img).toHaveAttribute('src', slides[1].imgUrl)
+    await user.click(nextButton)
+    expect(img).toHaveAttribute('src', slides[2].imgUrl)
+    await user.click(nextButton)
+    expect(img).toHaveAttribute('src', slides[0].imgUrl)
+  })
+
+  it('contains a previous button', () => {
+    render(<Carousel slides={slides}/>)
+    const prevButton = screen.getByTestId('prev-button')
+
+    expect(prevButton).toBeInTheDocument()
+  })
+
+  it('advances the slide when the Next button is clicked', async () => {
+    render(<Carousel slides={slides} />)
+    const img = screen.getByRole('img')
+    const nextButton = screen.getByTestId('prev-button')
+    const user = userEvent.setup()
+
+    await user.click(nextButton)
+    expect(img).toHaveAttribute('src', slides[2].imgUrl)
+    await user.click(nextButton)
+    expect(img).toHaveAttribute('src', slides[1].imgUrl)
+    await user.click(nextButton)
     expect(img).toHaveAttribute('src', slides[0].imgUrl)
   })
 });
--- carousel/src/Carousel.tsx
+++ carousel/src/Carousel.tsx
@@ -1,10 +1,23 @@
-import CarouselSlide from "./CarouselSlide.tsx";
-import {ReactNode} from "react";
+import CarouselSlide from './CarouselSlide'
+import {ReactNode, useState} from 'react'
+import CarouselButton from './CarouselButton'
 
 const Carousel = ({ slides }: {slides?: Slide[]}) => {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const setNextSlide = () => {
+    setActiveSlide((current) => (current + 1) % slides.length)
+  }
+
+  const setPrevSlide = () => {
+    setActiveSlide((current) => (current + slides.length - 1) % slides.length)
+  }
+
+
   return (
     <div data-testid="carousel" >
-      <CarouselSlide {...slides?.[0]} />
+      <CarouselSlide {...slides?.[activeSlide]} />
+      <CarouselButton data-testid={'next-button'} onClick={setNextSlide} value='Next' />
+      <CarouselButton data-testid={'prev-button'} onClick={setPrevSlide} value='Prev' />
     </div>
   )
 }
