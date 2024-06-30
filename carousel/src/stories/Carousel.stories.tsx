b60b453614ba69b9e3190c8e1f7447ea39a935c9
Randy Layne <randydavidl78@gmail.com>
2024-06-30 12:40:24 -0400

    :bug: Fix build errors related to TypeScript not finding toHaveStyleRule

--- carousel/src/stories/Carousel.stories.tsx
+++ carousel/src/stories/Carousel.stories.tsx
@@ -1,6 +1,7 @@
 import Carousel from "../Carousel";
 import {Meta, StoryObj} from "@storybook/react";
 import slides from "../examples/slides.tsx";
+import { useArgs } from '@storybook/preview-api'
 
 const meta = {
   title: 'Example/Carousel',
@@ -13,5 +14,16 @@ type Story = StoryObj<typeof meta>
 export const Basic: Story = {
   args: {
     slides
+  },
+  render: function RenderCarousel(args) {
+    const [, updateArgs] = useArgs()
+    return <Carousel
+      {...args}
+      onSlideIndexChange={(newSlideIndex) => {
+        updateArgs({slideIndex: newSlideIndex})
+        args.onSlideIndexChange?.(newSlideIndex)
+        }
+      }
+    />
   }
 }
